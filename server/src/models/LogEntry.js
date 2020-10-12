ed1ecc061ca755c6700bab6ef01a403cb113cf80
morhaham <moring3071@gmail.com>
2020-10-12 11:25:44 +0300

    disabled eslint and went all the way with prettier

--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
@@ -19,33 +19,40 @@ const requiredNumber = {
 // * Longitude - Number
 // * Created At - DateTime
 // * Updated At - DateTime
-const logEntrySchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const logEntrySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    comments: String,
+    image: String,
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 10,
+    },
+    latitude: {
+      ...requiredNumber,
+      min: -90,
+      max: 90,
+    },
+    longitude: {
+      ...requiredNumber,
+      min: -180,
+      max: 180,
+    },
+    visitDate: {
+      type: Date,
+      required: true,
+    },
   },
-  description: String,
-  comments: String,
-  image: String,
-  rating: {
-    type: Number,
-    min: 0,
-    max: 10,
-    default: 10,
-  },
-  latitude: {
-    ...requiredNumber, min: -90, max: 90,
-  },
-  longitude: {
-    ...requiredNumber, min: -180, max: 180,
-  },
-  visitDate: {
-    type: Date,
-    required: true,
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-const logEntry = mongoose.model('LogEntry', logEntrySchema);
+const logEntry = mongoose.model("LogEntry", logEntrySchema);
 module.exports = logEntry;
